8d069fc47bb97ae5e157e545ad240135fae5d2e4
Adrian <adrian@mxlinux.org>
2014-10-07 13:29:10 -0400

    update findshares version

--- mxfindshares.cpp
+++ mxfindshares.cpp
@@ -134,7 +134,7 @@ void mxfindshares::onStdoutAvailable() {
     QString out = ui->outputBox->toPlainText() + QString::fromUtf8(output);
     ui->outputBox->setPlainText(out);
     QScrollBar *sb = ui->outputBox->verticalScrollBar();
-    sb->setValue(sb->maximum());
+    //sb->setValue(sb->maximum());
 }
 
 
