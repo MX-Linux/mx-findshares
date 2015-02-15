cee6223708b5c4f1e1c249018c26925dc9f540fd
Adrian <adrian@mxlinux.org>
2015-02-15 12:02:08 -0500

    minor change

--- mxfindshares.h
+++ mxfindshares.h
@@ -6,20 +6,19 @@
  * Authors: Adrian
  *          MEPIS Community <http://forum.mepiscommunity.org>
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * MX Find Shares is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
- *****************************************************************************/
+ * You should have received a copy of the GNU General Public License
+ * along with MX Find Shares.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
 
 
 #ifndef MXFINDSHARES_H
@@ -36,7 +35,7 @@ class mxfindshares;
 class mxfindshares : public QDialog
 {
     Q_OBJECT
-    
+
 protected:
     QProcess *proc;
     QTimer *timer;
@@ -66,7 +65,7 @@ private slots:
     void on_buttonSave_clicked();
 
 private:
-    Ui::mxfindshares *ui;    
+    Ui::mxfindshares *ui;
 };
 
 #endif // MXFINDSHARES_H
